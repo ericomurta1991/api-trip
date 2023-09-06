cbef6120352e351082b7ab26e700d84c49b89a22
ericomurta1991 <erico_murta@yahoo.com.br>
2023-09-06 00:13:35 -0300

    imported the routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import StatusCodes from "http-status-codes"
 
 import db from "./config/dbConnect.js"
+import routes from "./routes/index.js";
 
 db.once("error", console.log.bind(console, "connection error"));
 db.once("open", () => {
@@ -14,5 +15,6 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
 
+routes(app);
 
 export default app;
