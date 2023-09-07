a6a20bc09a42da247d7ad14d75bf3d4db4d33eeb
ericomurta1991 <erico_murta@yahoo.com.br>
2023-09-07 00:52:00 -0300

    added user routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import  users from "../routes/user.Routes.js"
 
 
 const routes = (app) => {
@@ -7,7 +8,8 @@ const routes = (app) => {
     });
 
     app.use(
-        express.json()
+        express.json(),
+        users,
     );
 };
 
