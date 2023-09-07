ed242485689b8d43deac2a411828cc50a61c3a4b
ericomurta1991 <erico_murta@yahoo.com.br>
2023-09-07 00:56:07 -0300

    added user routes with http verbs

---
+++ src/routes/user.Routes.js
@@ -0,0 +1,14 @@
+import  express  from "express";
+import UserController from "../controllers/UserController.js";
+
+const router = express.Router();
+
+router
+    .get("/users", UserController.showUsers)
+    .get("/users/:id", UserController.showUsersById)
+    .post("/users", UserController.registerUser)
+    .put("/users/:id", UserController.updateUser)
+    .delete("/users/:id", UserController.deleteUser)
+
+
+export default router;    
